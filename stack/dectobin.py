b99c1f292c9c19f728d173bfe9c7849d1de3bc70
Stanislav Georgiev <sgeorgiev@mm-sol.com>
2020-02-04 17:08:32 +0200

    stack/dectobin.py: Added decimal to binary exercise.
    Signed-off-by: Stanislav Georgiev <sgeorgiev@mm-sol.com>

---
+++ stack/dectobin.py
@@ -0,0 +1,18 @@
+from Stack import *
+
+
+def decToBin(decNum):
+    binString = ""
+    remStack = Stack()
+
+    while decNum > 0:
+        rem = decNum % 2
+        remStack.push(rem)
+        decNum = decNum // 2
+
+    while not remStack.isEmpty():
+        binString = binString + str(remStack.pop())
+
+    return binString
+
+print(decToBin(2))
